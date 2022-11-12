59058c0e78a239ff89ac7760c75806f58ba09b03
azizulhakimaron <rrzaron@gmail.com>
2022-11-12 14:14:38 +0600

    Function Practice

---
+++ src/function_sum_two_num.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a+b
+x=float(input("First Number:"))
+y=float(input("Second Number:"))
+z=add(x,y)
+print("The Sum:",z)
+
+def add_three(a,b,c):
+    print(a+b+c)
+add_three(5,5,6)
---
+++ src/sum_indivisual digit.py
@@ -0,0 +1,5 @@
+a=input("The num:")
+sum=0
+for i in a:
+    sum=sum+int(i)
+print("The Sum:",sum) 
