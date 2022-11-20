230b7a082e0f647d9bf789a2fb12bbc6143dc563
azizulhakimaron <rrzaron@gmail.com>
2022-11-20 11:46:47 +0600

    Tkinter

---
+++ src/tkinter_Title.py
@@ -0,0 +1,5 @@
+from tkinter import *
+from tkinter import ttk
+root=Tk()
+root.title("Aron")
+root.mainloop()
---
+++ src/tkinter_level_pack.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import ttk
+root=Tk()
+root.title("Aron")
+label_1=ttk.Label(root,text='Enter Your Name:')
+label_1.pack()
+root.mainloop()
